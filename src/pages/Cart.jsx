116cdacfe1db929c4a7d10cfe6f9691d446ba9f5
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-09-27 14:46:54 +0300

    Removed date confirmed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -41,6 +41,36 @@ const Cart = () => {
               />
             </div>
 
+            <div className="py-6 w-[100%] border-b-[1px] border-[#E6E6E6] flex gap-3 flex-col">
+              <p className=" poppins-regular text-xl text-[#AD3989]">
+                Early Bird Ticket
+              </p>
+              <div className="flex justify-between w-[100%]">
+                <div className="w-[70%]">
+                  <p className="poppins-light text-[#4D4D4D]">
+                    This ticket gives you access to the 3-day in-person event at
+                    the Jua Kali Avenue, Nairobi
+                  </p>
+                  <p className="my-2 poppins-bold text-xl">Ksh 5,000</p>
+                </div>
+                <div className="w-[20%] flex flex-col gap-1 items-end">
+                  <div className="w-[100%] p-2 text-[#808080] poppins-light border-[#CCCCCC]  border-[1px] rounded-[2px] h-[40px] flex justify-between items-center">
+                    <p>-</p>
+                    <p>01</p>
+                    <p>+</p>
+                  </div>
+                  <p>Ksh 5,000</p>
+                </div>
+              </div>
+              <p className="text-[#545F71] poppins-light">
+                Who is this ticket for ?
+              </p>
+              <input
+                className="w-[60%] border-[1px] focus:border-gray-300 poppins-light focus:ring-gray-300  p-2 rounded-[6px] h-[48px] border-[#545F71]"
+                placeholder="James Macharia"
+              />
+            </div>
+
             <div className="flex text-[#333333] poppins-regular justify-between  w-[100%]">
               <p>Total Amount</p>
               <p>Ksh 19,000</p>
