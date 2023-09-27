560cf17db667cca8d243e70adf66868e0fa24093
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-09-27 07:26:42 +0300

    Added

---
+++ src/pages/Details.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+
+const Details = () => {
+  return (
+    <div className=" bg-[#F2F2F2]/50 py-12 pt-5">
+      <div className="w-[90%]  flex mx-auto">
+        <div className="w-[100%] flex justify-between items-start">
+          <div className="w-[30%] h-[377px] bg-white rounded-[8px] flex flex-col gap-4 p-4 border-[1px] border-[#E6E6E6] ">
+            <p>Event Information</p>
+            <div className="grid grid-cols-2 gap-4 w-[100%]">
+              <div className="flex flex-col gap-1">
+                <p>Start Date</p>
+                <p className="font-semibold">19th June 2024</p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <p>Start Date</p>
+                <p className="font-semibold">19th June 2024</p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <p>Start Date</p>
+                <p className="font-semibold">19th June 2024</p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <p>Start Date</p>
+                <p className="font-semibold">19th June 2024</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p>Location</p>
+              <p className="font-semibold">Jua Kali, Nairobi</p>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p>Hosted by</p>
+              <p className="font-semibold">Elixir Conf Africa</p>
+            </div>
+          </div>
+          <div className="w-[65%] h-[505px] bg-white rounded-[8px] border-[1px] border-[#E6E6E6]  flex flex-col gap-4 p-4 ">
+            <p className="font-bold text-xl">Available Tickets</p>
+            <div className="grid grid-cols-2 gap-8 w-[100%]">
+              <div className="w-[100%] border-[#E6E6E6] border-[1px] rounded-[8px]  h-[170px] flex flex-col justify-between p-3">
+                <div className="flex font-semibold justify-between w-[100%]">
+                  <p className="text-[#AD3989]">Early Bird Ticket</p>
+                  <p>200 Tickets</p>
+                </div>
+                <p>
+                  This ticket gives you access to the 3-day in-person event at
+                  the Jua Kali Avenue, Nairobi
+                </p>
+                <div className="flex font-semibold items-center justify-between w-[100%]">
+                  <p>KSH 5000 </p>
+                  <div className="flex gap-3 items-center">
+                    <button className="p-4 border-[1px] w-[73px] text-sm h-[34px] border-[#AD3989] text-[#AD3989]  flex gap-2 items-center justify-center rounded-[4px]">
+                      <p>+</p>
+                      <p>Cart</p>
+                    </button>
+                    <button className="p-4 border-[1px] w-[87px] text-xs h-[34px] bg-[#AD3989] text-white gap-2   flex items-center justify-center rounded-[4px]">
+                      Get <p>Ticket</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[100%] border-[#E6E6E6] border-[1px] rounded-[8px]  h-[170px] flex flex-col justify-between p-3">
+                <div className="flex font-semibold justify-between w-[100%]">
+                  <p className="text-[#AD3989]">Early Bird Ticket</p>
+                  <p>200 Tickets</p>
+                </div>
+                <p>
+                  This ticket gives you access to the 3-day in-person event at
+                  the Jua Kali Avenue, Nairobi
+                </p>
+                <div className="flex font-semibold items-center justify-between w-[100%]">
+                  <p>KSH 5000 </p>
+                  <div className="flex gap-3 items-center">
+                    <button className="p-4 border-[1px] w-[73px] text-sm h-[34px] border-[#AD3989] text-[#AD3989]  flex gap-2 items-center justify-center rounded-[4px]">
+                      <p>+</p>
+                      <p>Cart</p>
+                    </button>
+                    <button className="p-4 border-[1px] w-[87px] text-xs h-[34px] bg-[#AD3989] text-white gap-2   flex items-center justify-center rounded-[4px]">
+                      Get <p>Ticket</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[100%] border-[#E6E6E6] border-[1px] rounded-[8px]  h-[170px] flex flex-col justify-between p-3">
+                <div className="flex font-semibold justify-between w-[100%]">
+                  <p className="text-[#AD3989]">Early Bird Ticket</p>
+                  <p>200 Tickets</p>
+                </div>
+                <p>
+                  This ticket gives you access to the 3-day in-person event at
+                  the Jua Kali Avenue, Nairobi
+                </p>
+                <div className="flex font-semibold items-center justify-between w-[100%]">
+                  <p>KSH 5000 </p>
+                  <div className="flex gap-3 items-center">
+                    <button className="p-4 border-[1px] w-[73px] text-sm h-[34px] border-[#AD3989] text-[#AD3989]  flex gap-2 items-center justify-center rounded-[4px]">
+                      <p>+</p>
+                      <p>Cart</p>
+                    </button>
+                    <button className="p-4 border-[1px] w-[87px] text-xs h-[34px] bg-[#AD3989] text-white gap-2   flex items-center justify-center rounded-[4px]">
+                      Get <p>Ticket</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[100%] border-[#E6E6E6] border-[1px] rounded-[8px]  h-[170px] flex flex-col justify-between p-3">
+                <div className="flex font-semibold justify-between w-[100%]">
+                  <p className="text-[#AD3989]">Early Bird Ticket</p>
+                  <p>200 Tickets</p>
+                </div>
+                <p>
+                  This ticket gives you access to the 3-day in-person event at
+                  the Jua Kali Avenue, Nairobi
+                </p>
+                <div className="flex font-semibold items-center justify-between w-[100%]">
+                  <p>KSH 5000 </p>
+                  <div className="flex gap-3 items-center">
+                    <button className="p-4 border-[1px] w-[73px] text-sm h-[34px] border-[#AD3989] text-[#AD3989]  flex gap-2 items-center justify-center rounded-[4px]">
+                      <p>+</p>
+                      <p>Cart</p>
+                    </button>
+                    <button className="p-4 border-[1px] w-[87px] text-xs h-[34px] bg-[#AD3989] text-white gap-2   flex items-center justify-center rounded-[4px]">
+                      Get <p>Ticket</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
